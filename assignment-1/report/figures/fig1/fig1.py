b9d1b9e5315a8bd7c16cc260a0d2efab1bd931e6
Snehadeep-Gayen <cs21b078@smail.iitm.ac.in>
2024-08-23 18:36:23 +0530

    Completed report

--- assignment-1/report/figures/fig1/fig1.py
+++ assignment-1/report/figures/fig1/fig1.py
@@ -13,15 +13,15 @@ ax.set_axisbelow(True)
 ax.yaxis.grid(color='gray', linestyle='dashed')
 
 # draw bars with the number on top
-ax.bar(x - width/2, data[:, 0], width/2, label='100 connections', color='brown')
-ax.bar(x, data[:, 1], width/2, label='200 connections', color='purple')
-ax.bar(x + width/2, data[:, 2], width/2, label='300 connections', color='orange')
+ax.bar(x - width/2, 1-data[:, 0], width/2, label='100 connections', color='brown')
+ax.bar(x, 1-data[:, 1], width/2, label='200 connections', color='purple')
+ax.bar(x + width/2, 1-data[:, 2], width/2, label='300 connections', color='orange')
 
 # rects1 = ax.bar(x - width/2, data[:, 0], width/2, label='100')
 # rects2 = ax.bar(x, data[:, 1], width/2, label='200')
 # rects3 = ax.bar(x + width/2, data[:, 2], width/2, label='300')
 
-ax.set_ylabel('Admission Probability of a Connection')
+ax.set_ylabel('Blocking Probability of a Connection')
 ax.set_xticks(x)
 ax.set_xticklabels(labels)
 ax.legend()
